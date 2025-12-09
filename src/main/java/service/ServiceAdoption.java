d281e158c8ec9fec90fa25ddbf30b1e8bf219d66
ghosantos <gustavo.santos7611@gmail.com>
2025-12-09 20:41:24 -0300

    refactor: separa regras de negócio do ServiceAdoption e adiciona métodos de busca

--- src/main/java/service/ServiceAdoption.java
+++ src/main/java/service/ServiceAdoption.java
@@ -2,167 +2,76 @@ package service;
 
 import exceptions.DomainException;
 import model.Pet;
-import model.enums.PetGender;
+import model.enums.PetSex;
 import model.enums.PetType;
 import repository.PetRepository;
+import repository.PetRepositoryInMemory;
 import util.FileUtils;
-import util.InputUtils;
 
-import static util.InputUtils.NOT_INFORMED;
+import java.util.List;
 
 public class ServiceAdoption {
 
-    private final PetRepository petRepository = new PetRepository();
+    private final PetRepository petRepository;
 
-    public void registerNewPetForAdoption(){
-        //Exibe o formulário de cadastro do pet antes de iniciar as leituras
-        FileUtils.readForm();
-
-        String name;
-        while (true){
-            try {
-                System.out.print("\nDigite o nome completo do pet: ");
-                name = InputUtils.readName();
-                validateNoInvalidChars(name);
-                String[] parts = name.trim().split("\\s");
-
-                if (parts.length < 2){
-                    throw new DomainException("⚠️ Falta Sobrenome. Digite o nome completo (Ex: Alfredo Lactea).");
-                }
-
-                break;
-            }catch (DomainException e){
-                System.out.println(e.getMessage());
-            }
-        }
-
-        PetType petType;
-        while (true){
-            try {
-                System.out.print("Digite o tipo do pet (CACHORRO/GATO): ");
-                petType = PetType.valueOf(InputUtils.readPetType());
-                break;
-            }catch (IllegalArgumentException e){
-                System.out.println("⚠️ Opção inválida. Digite CACHORRO ou GATO");
-            }
-        }
-
-        PetGender petGender;
-        while (true){
-            try {
-                System.out.print("Digite o gênero do pet (MACHO ou FEMEA):");
-                petGender = PetGender.valueOf(InputUtils.readPetGender());
-                break;
-            }catch (IllegalArgumentException e){
-                System.out.println("️⚠️ Tipo inválido! Digite MACHO ou FEMEA.");
-            }
-        }
-
-        String address;
-        while (true){
-            try {
-                System.out.print("Endereço (Ex: Rua A, 100, São Paulo): ");
-                address = InputUtils.readAddress().trim();
-
-                String[] parts = address.split(",");
-
-                if (parts.length != 3) {
-                    throw new DomainException("⚠️ Deve conter exatamente 3 informações: Rua, Número e Cidade.");
-                }
-
-                String street = parts[0].trim();
-                String number = parts[1].trim();
-                String city = parts[2].trim();
-
-                if (street.isEmpty() || city.isEmpty()) {
-                    throw new DomainException("⚠️ Informe os dados obrigatórios (Rua e Cidade).");
-                }
-
-                if (number.isBlank()) {
-                    number = NOT_INFORMED; // Caso o usuario não informe o número
-                }
-
-                address = String.format("Rua: %s, Número: %s, Cidade: %s", street, number, city);
-                break;
-            }catch (DomainException e){
-                System.out.println(e.getMessage());
-            }
-        }
-
-        double petAge;
-        while (true){
-            try {
-                System.out.print("Idade aproximada do pet (em anos, use 0.X para meses): ");
-                petAge = InputUtils.readPetAge();
-
-                if (petAge > 20.0 || petAge <= 0.0){
-                    throw new DomainException("⚠️ Idade fora do limite. Digite um valor entre 0.1 e 20 anos. (0.1 representa 1 mês)");
-                }
-
-                if (petAge < 1.0){
-                    petAge = convertMonthsFractionToYears(petAge);
-                }
-                break;
-            }catch (DomainException e){
-                System.out.println(e.getMessage());
-            }catch (NumberFormatException e){
-                System.out.println("⚠️ Entrada Inválida! Digite um número válido.");
-            }
-        }
-
-        double petWeight;
-        while (true){
-            try {
-                System.out.print("Digite o peso do pet (em Kg): ");
-                petWeight = InputUtils.readPetWeight();
-
-                if (petWeight > 60 || petWeight < 0.5){
-                    throw new DomainException("Digite um peso entre 0.5 e 60Kg");
-                }
-
-                break;
-            }catch (DomainException e){
-                System.out.println(e.getMessage());
-            }catch (NumberFormatException e){
-                System.out.println("⚠️ Formato inválido. Digite apenas números para o peso (Ex: 15.5)");
-            }
-        }
-
-        String petRace;
-        while (true){
-            try {
-                System.out.print("Digite a raça do pet: ");
-                petRace = InputUtils.readPetRace();
-                validateNoInvalidChars(petRace);
-
-                break;
-            }catch (DomainException e){
-                System.out.println(e.getMessage());
-            }
-        }
-
-        Pet pet = new Pet(name, petType, petGender, address, petAge, petWeight, petRace);
-        petRepository.save(pet);
-
-        System.out.println("\n✔ Pet Cadastrado! ✔");
-
-        FileUtils.writeForm(pet);
+    public ServiceAdoption(){
+        this.petRepository = new PetRepositoryInMemory();
     }
 
-    // Valida se o nome contém apenas letras e espaços
-    private static void validateNoInvalidChars(String name) {
-        if (!name.matches("[a-zA-Z ]+")) {
-            throw new DomainException("⚠️ O nome não pode conter números ou caracteres especiais.");
+    public void registerNewPetForAdoption(Pet newPet){
+        if (petRepository.exists(newPet)){
+            throw new DomainException("Já existe um pet cadastrado com esse nome!\n");
         }
+
+        petRepository.save(newPet);
+        FileUtils.writeForm(newPet);
     }
 
-    // Se o valor for menor que 1, assume que é a idade em meses e converte para anos
-    private static double convertMonthsFractionToYears(double input) {
-        double months = input * 100; //Ex: 0.1 * 100 = 10 Meses
-        double years = months / 12; //Ex: 10 / 12 = 0.83 Anos
+    public List<Pet> searchPetsByName(String name, PetType petType){
+        return petRepository.findByNameAndType(name, petType);
+    }
 
-        // Faz o cast para double, pois Math.round retorna um long.
-        // Sem o cast, a divisão seria feita como inteira (long / int) e perderia as casas decimais, resultando em 0.
-        return (double) Math.round(years * 100) / 100;
+    public List<Pet> searchPetsBySex(PetSex petSex, PetType petType){
+        return petRepository.findBySexAndType(petSex, petType);
+    }
+
+    public List<Pet> searchPetsByAge(double age, PetType petType){
+        return petRepository.findByAge(age, petType);
+    }
+
+    public List<Pet> searchPetsByWeight(double weight, PetType petType){
+        return petRepository.findByWeight(weight, petType);
+    }
+
+    public List<Pet> searchPetsByRace(String name, PetType petType){
+        return petRepository.findByRace(name, petType);
+    }
+
+    public List<Pet> searchPetsByAddress(String address, PetType petType){
+        return petRepository.findByAddress(address, petType);
+    }
+
+    public void changePetName(String name, Pet pet){
+        pet.setName(name);
+    }
+
+    public void changePetAddress(String name, Pet pet){
+        pet.setAddress(name);
+    }
+
+    public void changePetAge(double age, Pet pet){
+        pet.setAge(age);
+    }
+
+    public void changePetWeight(double weight, Pet pet){
+        pet.setWeight(weight);
+    }
+
+    public void changePetRace(String race, Pet pet){
+        pet.setRace(race);
+    }
+
+    public void deletePet(Pet pet){
+        petRepository.delete(pet);
     }
 }
