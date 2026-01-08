191ea0265db066adcab6879a96beee4bc193ed99
ghosantos <gustavo.santos7611@gmail.com>
2026-01-08 00:59:58 -0300

    refactor: Mudança no nome dos métodos de busca pelo nome e sexo

--- src/main/java/repository/PetRepository.java
+++ src/main/java/repository/PetRepository.java
@@ -12,8 +12,8 @@ public interface PetRepository {
     void delete(Pet pet);
     boolean exists(Pet pet);
 
-    List<Pet> findByNameAndType(String name, PetType petType);
-    List<Pet> findBySexAndType(PetSex petSex, PetType petType);
+    List<Pet> findByName(String name, PetType petType);
+    List<Pet> findBySex(PetSex petSex, PetType petType);
     List<Pet> findByAge(double age, PetType petType);
     List<Pet> findByWeight(double weight, PetType petType);
     List<Pet> findByRace(String race, PetType petType);
